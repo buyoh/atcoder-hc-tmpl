8734bd8a60e7f73cebae535d5e8631203090c6c5
buyoh <japanjetone@live.jp>
2021-08-08 03:29:36 +0900

    change generator following ahc

--- bin/generator.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -eu
-cd `dirname $0`/..
-
-ruby src/generator/generator.rb
--- script/generate_testcases.sh
+++ script/generate_testcases.sh
@@ -5,6 +5,8 @@ cd `dirname $0`/..
 
 mkdir -p out/cases/
 
-for i in `seq 30`; do
-  bin/generator.sh > out/cases/in_${i}.txt
+i=0
+for f in `./bin/generator_multi.sh 30`; do
+  cp $f out/cases/in_${i}.txt
+  i=$(($i+1))
 done
