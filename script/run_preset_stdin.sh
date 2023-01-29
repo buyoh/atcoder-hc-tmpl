7649f19fa1d7776fec1dfcb1eee2b4d93627f6c8
buyoh <japanjetone@live.jp>
2023-01-29 17:01:28 +0900

    Use vis to calc score

--- script/run_preset_stdin.sh
+++ script/run_preset_stdin.sh
@@ -3,6 +3,11 @@
 set -eu
 cd `dirname $0`/..
 
+if [[ -e vis.html ]]; then
+  echo "./vis.html should not exist."
+  exit 1
+fi
+
 for FILE in `ls stdin`; do
   ID="${FILE%%.*}"
   FILE_STDIN="stdin/$FILE"
@@ -11,5 +16,11 @@ for FILE in `ls stdin`; do
 
   echo $FILE
   ./bin/run.sh < $FILE_STDIN > $FILE_STDOUT 2> $FILE_STDERR
+
+  # if CALC_SCORE_MYSELF; then
   tail -n1 $FILE_STDERR
+  # else
+  # ./third_party/tools/target/release/vis $FILE_STDIN $FILE_STDOUT
+  # mv vis.html out/$ID.vis.html
+  # fi
 done
