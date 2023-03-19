7e5be941eeaf73aede090dadd134279252022fd2
buyoh <buyoh@users.noreply.github.com>
2023-03-19 15:50:33 +0900

    Modifiy mount_ramdisk.sh for non-existing target dir

--- mount_ramdisk.sh
+++ mount_ramdisk.sh
@@ -3,23 +3,18 @@
 set -eu
 cd `dirname $0`/
 
-
-if [[ "`whoami`" != "root" ]]; then
-  echo "root is required"
-  exit 1
-fi
-
 set +e
 
-
 TARGET_DIR=out/cases_out
-umount $TARGET_DIR ||:
+mkdir -p $TARGET_DIR
+sudo umount $TARGET_DIR ||:
 rm -rf $TARGET_DIR
 mkdir $TARGET_DIR
-mount -t tmpfs -o size=1g tmpfs $TARGET_DIR
+sudo mount -t tmpfs -o size=1g tmpfs $TARGET_DIR
 
 TARGET_DIR=out/cases
-umount $TARGET_DIR ||:
+mkdir -p $TARGET_DIR
+sudo umount $TARGET_DIR ||:
 rm -rf $TARGET_DIR
 mkdir $TARGET_DIR
-mount -t tmpfs -o size=1g tmpfs $TARGET_DIR
+sudo mount -t tmpfs -o size=1g tmpfs $TARGET_DIR
