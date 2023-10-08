cf59d6f0d311e676a8befd37bb687e15928869ee
buyoh <15198247+buyoh@users.noreply.github.com>
2023-10-08 15:44:42 +0900

    Use score.sh

--- run_preset_stdin.sh
+++ run_preset_stdin.sh
@@ -19,14 +19,5 @@ for FILE in `ls stdin`; do
   echo $FILE
   ./bin/run.sh $ARGS < $FILE_STDIN > $FILE_STDOUT 2> $FILE_STDERR
 
-  # if CALC_SCORE_MYSELF; then
-  tail -n1 $FILE_STDERR
-  # TODO: visualize
-  # ./third_party/tools/target/release/vis $FILE_STDIN $FILE_STDOUT
-  # mv vis.png out/$ID.vis.png
-  # else
-  # ./third_party/tools/target/release/vis $FILE_STDIN $FILE_STDOUT
-  # cat vis.html src/vistool.html > out/$ID.vis.html
-  # rm vis.html
-  # fi
+  bash bin/score.sh $FILE_STDIN $FILE_STDOUT $FILE_STDERR out/$ID.vis.html
 done
